96271ee7fdcccbb7b2b7f3b3b02df3acb2fec519
Dzmitry <dimaau1@gmail.com>
2024-06-12 20:30:53 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { Link, Route, Routes } from "react-router-dom";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about"> About</Link>
+          </li>
+          <li>
+            <Link to="/projects"> Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact"> Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
