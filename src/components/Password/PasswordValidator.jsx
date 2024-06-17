ca105ecf91da5ba49eda09757503794984f1453b
Dzmitry <dimaau1@gmail.com>
2024-06-17 14:15:45 +0300

    add project password-validator (#1)

---
+++ src/components/Password/PasswordValidator.jsx
@@ -0,0 +1,45 @@
+import { evaluatePasswordStrength } from "../../utils/password";
+import { useState } from "react";
+import "./PasswordValidator.css";
+
+function PasswordValidator() {
+  const [password, setPassword] = useState("");
+  const [strength, setStrength] = useState("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const handleChange = (event) => {
+    const newPassword = event.target.value;
+    setPassword(newPassword);
+    console.log(newPassword);
+    const newStrength = evaluatePasswordStrength(newPassword);
+    setStrength(newStrength);
+  };
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible((prevState) => !prevState);
+  };
+
+  return (
+    <div className="password-validator">
+      <h2>Password Strength Validator</h2>
+      <div className="input-row">
+        <input
+          type={isPasswordVisible ? "text" : "password"}
+          value={password}
+          onChange={handleChange}
+          placeholder="Enter your password"
+        />
+        <button onClick={togglePasswordVisibility}>
+          {isPasswordVisible ? "Hide" : "Show"}
+        </button>
+      </div>
+      {strength && (
+        <p>
+          Password Strength: <strong>{strength}</strong>
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default PasswordValidator;
